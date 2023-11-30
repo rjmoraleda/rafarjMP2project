9c086e360a2a8b1564ea6408d415ecd4d3832e2f
rjmoraleda <rj.s.moraleda.dev@gmail.com>
2023-12-01 01:06:39 +0800

    emailjs
    fixed API

--- src/components/PageHeader.jsx
+++ src/components/PageHeader.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom'; 
+import './pageheader.css';
 
 const PageHeader = ({title, curPage}) => {
   return (
--- src/contact/ContactUs.jsx
+++ src/contact/ContactUs.jsx
@@ -1,55 +1,7 @@
-// // import React from 'react'
-// import PageHeader from '../components/PageHeader'
-// import React, { useRef } from 'react';
-// import emailjs from '@emailjs/browser';
-
-// export const ContactUs = () => {
-//   const form = useRef();
-
-//   const sendEmail = (e) => {
-//     e.preventDefault();
-
-//     emailjs.sendForm('YOUR_SERVICE_ID', 'YOUR_TEMPLATE_ID', form.current, 'YOUR_PUBLIC_KEY')
-//       .then((result) => {
-//           console.log(result.text);
-//       }, (error) => {
-//           console.log(error.text);
-//       });
-//   };
-
-// const ContactUs = () => {
-//   return (
-//     <div>
-//       <PageHeader title={"Get In Touch With Us"} curPage={"Contact Us"}/>
-//       <div className='map-address-section padding section-bg'>
-//             <div className="container">
-//                 <div className="section-header text-center">
-//                     <span className='subtitle'>{subTitle}</span>
-//                       <h2 className='title'>{title}</h2>
-//                       {/* <form ref={form} onSubmit={sendEmail}>
-//       <label>Name</label>
-//       <input type="text" name="user_name" />
-//       <label>Email</label>
-//       <input type="email" name="user_email" />
-//       <label>Message</label>
-//       <textarea name="message" />
-//       <input type="submit" value="Send" />
-//     </form> */}
-//                 </div>
-//                 </div>
-//                 </div>
-//     </div>
-//   )
-// }
-// }
-
-// export default ContactUs
-
-
-
 import React, { useRef } from 'react';
 import emailjs from '@emailjs/browser';
 import PageHeader from '../components/PageHeader';
+import './contact.css'
 
 const ContactUs = () => {
   const form = useRef();
@@ -68,7 +20,7 @@ const ContactUs = () => {
   return (
     <div>
     <PageHeader title={"Get In Touch With Us"} curPage={"Contact Us"}/>
-      <div className='map-address-section padding section-bg'>
+      <div className='contact section-bg'>
             <div className="container">
             <form ref={form} onSubmit={sendEmail}>
        <label>Name</label>
@@ -78,10 +30,6 @@ const ContactUs = () => {
        <textarea name="message" />
        <input type="submit" value="Send" />
      </form>
-                {/* <div className="section-header text-center">                     
-                <span className='subtitle'>{subTitle}</span>
-                       <h2 className='title'>{title}</h2>
-              </div> */}
         </div>
     </div>
     </div>
